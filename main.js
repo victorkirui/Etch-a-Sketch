5102f46a60be7c5f0ace4a68ea46b4b075629b64
victorkirui <vkkiprop@gmail.com>
2024-07-26 08:24:46 +0300

    Refactor main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const btn = document.querySelector("#btn");
 const container = document.querySelector("#container");
 
-//This function creates grids of 16x16. creates the number of grids inputed by user
+//This function creates grids of 16x16. creates the number of grids added by user
 function createGrids(num) {
-  let divsArr = [];
+  let divsArray = [];
   let screenWidth = window.innerWidth;
   let screenHeight = window.innerHeight;
 
@@ -12,14 +12,14 @@ function createGrids(num) {
     div.style.width = `${num}px`;
     div.style.height = `${num}px`;
     div.setAttribute("class", "grid");
-    divsArr.push(div);
+    divsArray.push(div);
   }
 
-  divsArr.forEach((div) => {
+  divsArray.forEach((div) => {
     container.appendChild(div);
   });
 
-  //Selecting the grids using it's classname
+  //Selecting the grids using it's className
   const grids = document.querySelectorAll(".grid");
 
   addHoverEffects(grids);
@@ -40,7 +40,12 @@ function addHoverEffects(grids) {
 
   gridsArray.forEach((grid) => {
     grid.addEventListener("mouseenter", () => {
-      grid.style.background = " red";
+      grid.style.background = `rgb(255,36,${x})`;
+      // grid.style.background = "hsl(89, 24%, 69%)";
+    });
+
+    grid.addEventListener("mouseleave", () => {
+      grid.style.background = "none";
     });
   });
 }
